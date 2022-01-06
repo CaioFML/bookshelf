2ac24e0421b233a3e9b1bb2eae407934f874d509
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2022-01-06 19:05:26 -0300

    Create mutation for create author

---
+++ app/graphql/mutations/create_author.rb
@@ -0,0 +1,12 @@
+class Mutations::CreateAuthor < GraphQLMutations::BaseMutation
+  null true
+
+  argument :first_name, String, required: false
+  argument :last_name, String, required: false
+  argument :yob, Int, required: false
+  argument :is_alive, Boolean, required: false
+
+  def resolve(first_name:, last_name:, yob:, is_alive:)
+    Author.create(first_name: first_name, last_name: last_name, yob: yob, is_alive: is_alive)
+  end
+end
